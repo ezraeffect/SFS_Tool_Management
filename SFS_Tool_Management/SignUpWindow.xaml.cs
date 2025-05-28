1a61aa5c1b457fc257f6e85f967777bc3720698f
CHK404 <azbxabcd4@gmail.com>
2025-05-28 10:16:06 +0900

    ~ing

--- SFS_Tool_Management/SignUpWindow.xaml.cs
+++ SFS_Tool_Management/SignUpWindow.xaml.cs
@@ -58,7 +58,7 @@ namespace SFS_Tool_Management
             }
             if (!(Regex.IsMatch(PN, @"^010\d{8}$") || Regex.IsMatch(PN, @"^0[2-6]\d{7,8}$")))
             {
-                MessageBox.Show("올바른 전화번호 형식이 아닙니다. 다시 입력해주세요.");
+                MessageBox.Show("올바른 전화번호 형식이 아닙니다.");
                 return;
             }
             if (string.IsNullOrWhiteSpace(id))
@@ -66,9 +66,9 @@ namespace SFS_Tool_Management
                 MessageBox.Show("아이디를 입력하세요.");
                 return;
             }
-            if (id.Length < 8 && id.Length > 16)
+            if (id.Length < 8 || id.Length > 16)
             {
-                MessageBox.Show("아이디는 8자 이상 16자 이하여야 합니다. 다시 입력해주세요.");
+                MessageBox.Show("아이디는 8자 이상 16자 이하여야 합니다.");
                 return;
             }
             if (string.IsNullOrWhiteSpace(pw))
@@ -76,10 +76,25 @@ namespace SFS_Tool_Management
                 MessageBox.Show("비밀번호를 입력하세요.");
                 return;
             }
-            if (Regex.IsMatch(pw, @"^(?!.*[가-힣])[a-zA-Z0-9]+$"))    //~ing
-            if (pw.Length < 8)
+            if (pw.Length < 8 || pw.Length > 20)
             {
-                MessageBox.Show("비밀번호가 너무 짧습니다.");
+                MessageBox.Show("비밀번호는 8자 이상 20자 이하여야 합니다.");
+                return;
+            }
+            if (!Regex.IsMatch(pw, @"[^가-힣ㄱ-ㅎㅏ-ㅣ]"))
+            {
+                MessageBox.Show("비밀번호는 대문자, 소문자, 특수문자로만 구성되어야 합니다.");
+                return;
+            }
+            if (!Regex.IsMatch(pw, @"\d"))
+            {
+                MessageBox.Show("비밀번호에는 최소 하나 이상의 숫자가 포함되어야 합니다.");
+                return;
+            }
+            if (!Regex.IsMatch(pw, @"[~․!@#$%^&*()_\-+={}[\]|\:;""<>,.?/]$"))
+            {
+                MessageBox.Show("비밀번호에는 최소 하나 이상의 특수문자가 포함되어야 합니다.");
+                return;
             }
         }
     }
