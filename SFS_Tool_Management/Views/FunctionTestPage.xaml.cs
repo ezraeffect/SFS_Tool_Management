d3726b312a7d237db15cf038c722cc1c7899d4a4
ezraeffect <build@fx0.kr>
2025-05-28 13:50:56 +0900

    Temp: Add DB Connection

---
+++ SFS_Tool_Management/Views/FunctionTestPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using SFS_Tool_Management.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace SFS_Tool_Management.Views
+{
+    /// <summary>
+    /// FunctionTestPage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class FunctionTestPage : Page
+    {
+        public SQLcInfo cInfo = new SQLcInfo() {
+            Server = "sfstool.database.windows.net",
+            Port = "1433",
+            Database = "SFS",
+            Uid = "Codingon",
+            PWD = "sfs2751!"
+        };
+
+        public FunctionTestPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button_ConnectSQL_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection conn = new(cInfo.GetConnectInfo());
+            conn.Open();
+
+            string query = "SELECT * FROM Persons";
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    ReadSingleRow(reader);
+                }
+            }
+        }
+        public void ReadSingleRow(IDataRecord record)
+        {
+            textBlock_debug.Text += String.Format("{0}, {1}", record[0], record[1]);
+            textBlock_debug.Text += "\r\n";
+        }
+    }
+}
