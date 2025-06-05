dc133b87d1f7d9a205f4ae89a3ae114e43e6e6b9
Jeonghun Lee <build@fx0.kr>
2025-06-05 09:25:37 +0900

    Feat: ToastMessage

---
+++ SFS_Tool_Management/Styles/ToastMessage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SFS_Tool_Management.Styles
+{
+    /// <summary>
+    /// ToastMessage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ToastMessage : UserControl
+    {
+        public ToastMessage()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(Geometry), typeof(ToastMessage));
+        public Geometry Icon
+        {
+            get => (Geometry)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        public static readonly DependencyProperty ColorProperty =
+            DependencyProperty.Register("Color", typeof(Brush), typeof(ToastMessage));
+
+        public Brush Color
+        {
+            get => (Brush)GetValue(ColorProperty);
+            set => SetValue(ColorProperty, value);
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(ToastMessage));
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+    }
+}
---
+++ SFS_Tool_Management/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace SFS_Tool_Management.ViewModels
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        string? toastMessage;
+
+        [ObservableProperty]
+        Geometry? toastGeometry;
+        
+        public void ShowToastView(string msg, Geometry ico)
+        {
+            
+        }
+
+        public void ShowToastView(string msg, string icoPath)
+        {
+            // 매개변수의 값이 Null이거나 공란이 아니라면,
+            if (!string.IsNullOrWhiteSpace(msg) && !string.IsNullOrWhiteSpace(icoPath))
+            {
+                // Path String을 Geometry 객체로 Parse한다.
+                Geometry ico = Geometry.Parse(icoPath);
+            }
+            
+        }
+
+        public void CloseToastView()
+        {
+
+        }
+    }
+}
